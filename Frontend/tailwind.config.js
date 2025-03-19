6a4526c5e3a4f9e4bdcf71f71da32f0423ee984e
Pankaj Suman <pankajsuman806041@gmail.com>
2025-03-19 12:42:49 +0530

    Welcome To deepak

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 1.5s ease-out",
+        typing: "typing 6s steps(20) infinite"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" }
+        },
+        typing: {
+          "0%": { width: "0" },
+          "20%": { width: "100%" },
+          "80%": { width: "100%" },
+          "100%": { width: "0" }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
